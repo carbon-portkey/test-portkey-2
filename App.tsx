7577dda98c5fc7d891e9aa4df5531cd00d1911aa
thiendangvan <thiendangvan@ncc.asia>
2023-12-27 19:19:27 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import "expo-dev-client";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { portkey } from "@portkey/react-native-sdk";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="get wallet info" onPress={portkey.login} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
